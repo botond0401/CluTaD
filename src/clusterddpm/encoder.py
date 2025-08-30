acbc687a8a18b140812e8a69726461f67157a333
Botond Kovacs <botond.kov0401@gmail.com>
2025-08-30 16:48:28 +0200

    encoder - minor changes

--- src/clusterddpm/encoder.py
+++ src/clusterddpm/encoder.py
@@ -10,7 +10,7 @@ class TabularEncoder(nn.Module):
     """
     def __init__(self, input_dim, hidden_dims, latent_dim):
         super().__init__()
-        
+
         layers = []
         prev_dim = input_dim
         for h_dim in hidden_dims:
@@ -18,15 +18,15 @@ class TabularEncoder(nn.Module):
             layers.append(nn.ReLU())
             prev_dim = h_dim
         self.feature_extractor = nn.Sequential(*layers)
-        
+
         self.mu_layer = nn.Linear(prev_dim, latent_dim)
         self.logvar_layer = nn.Linear(prev_dim, latent_dim)
-        
+
     def forward(self, x):
         """
         Args:
             x: (batch_size, input_dim)
-            
+
         Returns:
             mu: (batch_size, latent_dim)
             logvar: (batch_size, latent_dim)
